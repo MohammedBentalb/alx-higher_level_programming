44252a7e12a790da9fff700f49cd919e5068ee04
mohammed bentalb <mohammedbentalb0@gmail.com>
2023-06-06 17:50:41 +0100

    fix

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,10 +3,10 @@ import random
 number = random.randint(-10000, 10000)
 n = abs(number) % 10
 if number < 0:
-   n = -n
+    n = -n
 if n > 5:
-   print(f"Last digit of {number} is {n} and it is greater than 5")
+    print(f"Last digit of {number} is {n} and it is greater than 5")
 elif n < 6 and n != 0:
-   print(f"Last digit of {number} is {n} and it is less than 6 and not 0")
+    print(f"Last digit of {number} is {n} and it is less than 6 and not 0")
 else:
-   print(f"Last digit of {number} is {n} and is 0")
+    print(f"Last digit of {number} is {n} and is 0")
--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -2,4 +2,4 @@
 ''' printing the alphabet usig ascii inside the loop'''
 
 for i in range(ord('a'), ord('z') + 1):
-   print("{}".format(chr(i)), end = "")
+    print("{}".format(chr(i)), end="")

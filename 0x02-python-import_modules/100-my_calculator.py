793fd7246ec7180a50d622c62ba562f565d2f374
mohammed bentalb <mohammedbentalb0@gmail.com>
2023-06-08 16:20:43 +0100

    fix

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    from calculator_1 import add, sub, div, mul
+
+    ln = len(sys.argv) - 1
+
+    if ln != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    a = int(sys.argv[1])
+    op = sys.argv[2]
+    b = int(sys.argv[3])
+    match op:
+        case "+":
+            print("{:d} {:s} {:d} = {:d}".format(a, op, b, add(a, b)))
+        case "-":
+            print("{:d} {:s} {:d} = {:d}".format(a, op, b, sub(a, b)))
+        case "*":
+            print("{:d} {:s} {:d} = {:d}".format(a, op, b, mul(a, b)))
+        case "/":
+            print("{:d} {:s} {:d} = {:d}".format(a, op, b, div(a, b)))
+        case _:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)

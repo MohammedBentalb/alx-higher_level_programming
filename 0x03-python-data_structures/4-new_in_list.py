9781829ed26b207ec27c763effec229077052f2b
Mohammed Bentalb <mohammedbentalb0@gmail.com>
2023-06-10 19:11:30 +0100

    fix

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    new_list = my_list.copy()
+    if my_list:
+        if idx > len(my_list) - 1 or idx < 0:
+            return new_list
+        else:
+            new_list[idx] = element
+            return new_list

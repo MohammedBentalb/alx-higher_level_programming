60db41483e06877f07d51cb9214c1c75c885199d
Mohammed Bentalb <mohammedbentalb0@gmail.com>
2023-06-10 20:04:31 +0100

    fix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for col in row:
+            print("{}".format(col), end=" " if col != row[-1] else "" )
+        print()

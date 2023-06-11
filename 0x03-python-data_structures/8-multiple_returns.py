1f9135217681601ec4a8e1ae94be3a1cfa187b97
Mohammed Bentalb <mohammedbentalb0@gmail.com>
2023-06-11 15:03:15 +0100

    fix

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence == "None":
+    if len(sentence) == 0:
         tuple_1 = (0, "None")
     else:
         tuple_1 = (len(sentence), sentence[0])

5b17222e09725b6b4e109fcc2cd46e473cce551f
mohammed bentalb <mohammedbentalb0@gmail.com>
2023-06-26 16:43:53 +0100

    fix

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     sum = 0
     for i in range(x):
         try:
-            print("{}".format(my_list[i]), end="");
+            print("{}".format(my_list[i]), end="")
             sum += 1
         except IndexError:
             break

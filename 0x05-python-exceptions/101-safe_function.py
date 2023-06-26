2160f88f135eba2b3782974d6fdeb8b80c41f162
mohammed bentalb <mohammedbentalb0@gmail.com>
2023-06-26 20:10:15 +0100

    fix

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -7,5 +7,8 @@ def safe_function(fct, *args):
         new = fct(*args)
     except Exception as e:
         sys.stderr.write("Exception: {}\n".format(e))
+        '''
+        print("Exception: {}".format(e), file=sys.stderr)
+        '''
         return None
     return new

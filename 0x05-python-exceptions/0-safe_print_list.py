5e241db8c6b24a13ebd3972b701b90e95f675c7a
mohammed bentalb <mohammedbentalb0@gmail.com>
2023-06-26 16:40:05 +0100

    fix

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    sum = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="");
+            sum += 1
+        except:
+            break
+    print()
+    return sum
+

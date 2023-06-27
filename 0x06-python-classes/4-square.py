f6af73d3953cd1deae26b225d3883c9a968d3f5b
Mohammed Bentalb <mohammedbentalb0@gmail.com>
2023-06-27 18:51:23 +0100

    fix

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""class"""
+
+
+class Square:
+    """init"""
+
+    def __init__(self, size=0):
+        """set"""
+
+        if type(size) is int:
+            if size < 0:
+                raise ValueError('size must be >= 0')
+            else:
+                self.__size = size
+        else:
+            raise TypeError('size must be an integer')
+
+    def area(self):
+        return self.__size ** 2
